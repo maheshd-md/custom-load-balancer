95f93518b6b13568f55d49b49ec00c5edaf470fb
maheshd-md <maheshdh1993@gmail.com>
2025-09-21 03:07:41 +0400

    initial commit

---
+++ src/test/java/com/example/lb/CustomLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
